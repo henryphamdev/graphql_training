453c10106b38954cfbced9f9e4682c8e147bf8fb
thanh.pham <thanh.pham@helloxom.vn>
2018-11-27 17:07:22 +0700

    noi chung la deo hieu

--- src/index.js
+++ src/index.js
@@ -1,66 +1,54 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-// 1
-let links = [{
-        "id": "link-0",
-        "description": "Fullstack tutorial for GraphQL",
-        "url": "www.howtographql.com"
-      },
-      {
-        "id": "link-1",
-        "description": "bao dan tri báo tiếng việt của người việt",
-        "url": "https://dantri.com.vn/"
-      },
-      {
-        "id": "link-2",
-        "description": "eo gi day",
-        "url": "1234567890"
-      },
-      {
-        "id": "link-3",
-        "description": "eo gi day",
-        "url": "1234567890"
-      }];
-	
-let idCount = links.length;
+const { Prisma } = require('prisma-binding')
 const resolvers = {
 	Query: {
 	  	info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,
-		link: (root, data) => {
-			let index_key = data.id.split('-')[1];
-			return links[index_key];
-		}
+		feed: (root, args, context, info) => {
+			return context.db.links({}, info)
+		},
+		// link: (root, data) => {
+		// 	let index_key = data.id.split('-')[1];
+		// 	return links[index_key];
+		// }
 	},
 	Mutation: {
     // 2
-    post: (root, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+    	//return context.db.mutation.createLink(data_create	, info);
+       	return context.db.mutation.createLink({
+       		data : {
+       			url : args.url,
+       			description : args.description
+       		}
+       	}, info);
     },
-    updateLink : (root, data) => {
-    	let update_element = links[data.id.split('-')[1]];
-    	update_element.url = data.url;
-    	update_element.description = data.description;
-    	return update_element;
-    },
-    deleteLink : (root, data) => {
-    	if(links[data.id.split('-')[1]]){
-    		let temp = links[data.id.split('-')[1]];
-    		delete links[data.id.split('-')[1]];
-    		return temp;
-    	}
-    }
+    // updateLink : (root, data) => {
+    // 	let update_element = links[data.id.split('-')[1]];
+    // 	update_element.url = data.url;
+    // 	update_element.description = data.description;
+    // 	return update_element;
+    // },
+    // deleteLink : (root, data) => {
+    // 	if(links[data.id.split('-')[1]]){
+    // 		let temp = links[data.id.split('-')[1]];
+    // 		delete links[data.id.split('-')[1]];
+    // 		return temp;
+    // 	}
+    // }
   },
 }
 // 3
 const server = new GraphQLServer({
   typeDefs  : './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/henrypham/database/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  })
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
